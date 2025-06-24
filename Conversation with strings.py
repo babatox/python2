cdf82828cbec5721a876c0197cead91edb03c2b4
abigail <iyalolaoluwatosin@gmail.com>
2025-06-24 19:40:33 +0100

    Add files via upload

---
+++ Conversation with strings.py
@@ -0,0 +1,13 @@
+name="pranks"
+age=10
+is_student=false
+weight=25.67
+
+print("Name:", name)
+print("Type of name:", type(name))
+print("Age:", age)
+print("Type of age:", type(age))
+print("Is Student:", is_student)
+print("Type of is_student:", type(is_student))
+print("Weight:", weight)
+print("Type of weight:", type(weight))
