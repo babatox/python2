dfab6bf145d2986ff6075941c1e26a949d196f91
abigail <iyalolaoluwatosin@gmail.com>
2025-06-24 18:48:17 +0100

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,20 @@
+name="abigail"
+age=12
+is_student=True
+weight=25.67
+
+print("Name:", name)
+print("Type of name:", type(name))
+print("Age:", age)
+print("Type of age:", type(age))
+print("Is Student:", is_student)
+print("Type of is_student:", type(is_student))
+print("Weight:", weight)
+print("Type of weight:", type(weight))
+
+
+print("in after type casting....")
+age = str(age)
+print("Age after type casting:", age)
+
+
