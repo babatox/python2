0d08a8ad87b6fb0e74741b3ac2c101e39bdd388b
abigail <iyalolaoluwatosin@gmail.com>
2025-06-24 19:03:51 +0100

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,10 @@
+num1=45
+num2=2
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Floor Division:", num1 // num2)
+print("Modulus:", num1 % num2)
+print("square=",num1**2)
+print("square root:", square**0.5)
