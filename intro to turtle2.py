133e91d3894a71711418f4fd3987e0c94032099f
abigail <iyalolaoluwatosin@gmail.com>
2025-06-26 18:44:30 +0100

    Add files via upload

---
+++ intro to turtle2.py
@@ -0,0 +1,24 @@
+import turtle
+turtle.Screen().bgcolor("lightpurple")
+board=turtle.Turtle()
+
+# Triangle
+ 
+board.left(120)
+board.forward(100)
+ 
+board.left(120)
+board.forward(100)
+#square
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)board.forward(100)
+board.left(90)
+
