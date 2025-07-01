1d28fd2abf13291afb8d19a440d3ab16b7c28916
abigail <iyalolaoluwatosin@gmail.com>
2025-07-01 18:28:09 +0100

    Add files via upload

---
+++ pos negative num.py
@@ -0,0 +1,8 @@
+number =int(input("Enter a number to check "))
+print("number to checked:", number)
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is neutral.")
