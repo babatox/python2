3bae36ea16dd41a7f48ea65ae76df37bd3670d5a
abigail <iyalolaoluwatosin@gmail.com>
2025-07-26 17:19:24 +0100

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,13 @@
+#Basic star pattern
+print ("star patterns /n")
+for i in range(1,6):
+    for j in range(i):
+        print("*", end="")
+    print("/n")
+
+# inverted star pattern
+print ("inverted star patterns /n")
+for i in range(6, 1, -1):
+    for j in range(i,1,-1):
+        print("*", end="")
+    print("/n")
